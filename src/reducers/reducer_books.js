9cc6586751152b20921e7b4d0e9e7048e839359d
Andres Vizcaino <andruvizcaino1989@gmail.com>
2017-04-30 23:36:41 -0500

    add reducers, actions and containers

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+  return [
+    { title: 'Javascript the good parts', pages: 101 },
+    { title: 'Harry potter', pages: 30 },
+    { title: 'The dark tower', pages: 85 },
+    { title: 'Eloquent ruby', pages: 1 }
+  ]
+}
